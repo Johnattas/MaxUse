0e59e519763367d50ac3d37d87f59c4f4f1af869
Johnattas Santana <johnattas@gmail.com>
2026-05-05 13:27:35 -0300

    feat: atualizando componentes

--- src/Routes/getRoute.ts
+++ src/Routes/getRoute.ts
@@ -2,15 +2,15 @@ import { useRoute } from 'ziggy-js';
 import { type MaybeRefOrGetter, toValue } from 'vue';
 import { isBlank } from '@/Helpers/Types';
 
-export const getRoute = (routeName: MaybeRefOrGetter<string | null> = null, data: any = {} ) => {
+export const getRoute = (routeName: MaybeRefOrGetter<string | null> = null, data: any = {}): string | null => {
     const route_value = toValue(routeName);
-    if (!route_value || isBlank(route_value)) return;
+    if (!route_value || isBlank(route_value)) return null;
 
     const data_value = toValue(data) ?? {};
 
     const route = useRoute();
     if (route().has(route_value)) return route(route_value, data_value);
-    return routeName;
+    return null;
 };
 
-export const getRouteName = getRoute;
+export const getRouteByName = getRoute;
