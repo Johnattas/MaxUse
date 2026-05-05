530e66d003b92eb17f8cdf37562f9bcb81e2d73e
Johnattas Santana <johnattas@gmail.com>
2026-05-05 12:25:38 -0300

    feat: atualizando componentes

---
+++ src/Routes/getRoute.ts
@@ -0,0 +1,13 @@
+import { useRoute } from 'ziggy-js';
+import { type MaybeRefOrGetter, toValue } from 'vue';
+import { isBlank } from '@/Helpers/Types';
+
+export const getRoute = (routeName: MaybeRefOrGetter<string | null> = null, data: any = {} ) => {
+    const route_value = toValue(routeName);
+    if (!route_value || isBlank(route_value)) return;
+
+    const data_value = toValue(data) ?? {};
+
+    const route = useRoute();
+    if (route().has(route_value)) return route(route_value, data_value);
+};
--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -3,3 +3,4 @@ export * from './apiPostRoute';
 export * from './apiPutRoute';
 export * from './apiDeleteRoute';
 export * from './apiUploadRoute';
+export * from './getRoute';
